166c9ea1bff2be06d501b1fffb7448ce0656e0e0
yihuang <huangyi@obsbot.com>
2025-09-12 18:45:41 +0800

    0912

--- GeeRPC/client_test.go
+++ GeeRPC/client_test.go
@@ -3,6 +3,8 @@ package geerpc
 import (
 	"context"
 	"net"
+	"os"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -37,7 +39,7 @@ func (b Bar) Timeout(argv int, reply *int) error {
 func startServer(addr chan string) {
 	var b Bar
 	_ = Register(&b)
-	l, _ := net.Listen("tcp", ":6666")
+	l, _ := net.Listen("tcp", ":8888")
 	addr <- l.Addr().String()
 	Accept(l)
 }
@@ -63,4 +65,23 @@ func TestClient_Call(t *testing.T) {
 		err := client.Call(context.Background(), "Bar.Timeout", 1, &reply)
 		_assert(err != nil && strings.Contains(err.Error(), "handle timeout"), "expect a timeout error")
 	})
+}
+
+func TestXDial(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		ch := make(chan struct{})
+		addr := "/tmp/geerpc.sock"
+		go func() {
+			_ = os.Remove(addr)
+			l, err := net.Listen("unix", addr)
+			if err != nil {
+				t.Fatal("failed to listen unix socket")
+			}
+			ch <- struct{}{}
+			Accept(l)
+		}()
+		<-ch
+		_, err := XDial("unix@" + addr)
+		_assert(err == nil, "failed to connect unix socket")
+	}
 }
--- GeeRPC/main/main.go
+++ GeeRPC/main/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"geerpc"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -18,46 +19,40 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := geerpc.Register(&foo); err != nil {
-		log.Fatal("register error: ", err)
-	}
-
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error: ", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	geerpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = geerpc.Register(&foo)
+	geerpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-
-	client, _ := Dial("tcp", <-addr)
-	defer func() {
-		_ = client.Close()
-	}()
+func call(addrCh chan string) {
+	client, _ := geerpc.DialHTTP("tcp", <-addrCh)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
+	// send request & receive response
 	var wg sync.WaitGroup
-
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
