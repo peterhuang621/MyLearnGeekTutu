0c0e7580415c438584c967f38ac23f586539bfee
yihuang <ehuang986@outlook.com>
2025-09-18 00:40:42 +0800

    0918

---
+++ GeeRPC/xclient/discovery.go
@@ -0,0 +1,105 @@
+package xclient
+
+import (
+	"errors"
+	. "geerpc"
+	"io"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRobinSelect
+)
+
+type Discover interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+type MultiServerDiscovery struct {
+	r       *rand.Rand
+	mu      sync.RWMutex
+	servers []string
+	index   int
+}
+
+func (d *MultiServerDiscovery) Refresh() error {
+	return nil
+}
+
+func (d *MultiServerDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+func (d *MultiServerDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+func (d *MultiServerDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServerDiscovery {
+	d := &MultiServerDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discover = (*MultiServerDiscovery)(nil)
+
+type XClient struct {
+	d       Discover
+	mode    SelectMode
+	opt     *Option
+	mu      sync.Mutex
+	clients map[string]*Client
+}
+
+var _ io.Closer = (*XClient)(nil)
+
+func NewXClient(d Discover, mode SelectMode, opt *Option) *XClient {
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*Client)}
+}
+
+func (xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	for key, client := range xc.clients {
+		_ = client.Close()
+		delete(xc.clients, key)
+	}
+	return nil
+}
